e464a57582dcf65a9531ca7491d56eceaaa08a09
Ferdis17 <fmuriyesu@users.noreply.github.com>
2018-10-14 11:30:08 -0400

    domain and controller
    domain, controller and config

---
+++ AngularJSWithSpring/src/main/java/angular/spring/controllers/UserController.java
@@ -0,0 +1,31 @@
+package angular.spring.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import angular.spring.domain.User;
+
+@RestController
+public class UserController {
+	
+	private static Logger logger = Logger.getLogger(UserController.class);
+	
+	@GetMapping(value = "/welcome")
+	public ModelAndView home() {
+		return new ModelAndView("welcome");
+	}
+	
+	@GetMapping(value = "/userList")
+	public @ResponseBody User getDetails() {
+		
+		logger.info("Fetching user details ...!");
+		User user = new User();
+		user.setName("Tinga");
+		user.setDepartment("compro");
+		logger.info("User details are: " + user.toString());
+		return user;
+	}
+}
